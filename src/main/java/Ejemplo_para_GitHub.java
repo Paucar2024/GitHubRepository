12bc9ca6964e9594e1b7d25a6481e8d8d827a481
Zoyla12345 <142934275+Zoyla12345@users.noreply.github.com>
2024-08-16 16:46:04 -0500

    Update Ejemplo_para_GitHub.java
    Esta es una actualización de clase para ejemplo de GitHub

--- src/main/java/Ejemplo_para_GitHub.java
+++ src/main/java/Ejemplo_para_GitHub.java
@@ -3,6 +3,7 @@ public class Ejemplo_para_GitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Este es una modificación al archivo de ejemplo de GitHub
 
 	}
 
